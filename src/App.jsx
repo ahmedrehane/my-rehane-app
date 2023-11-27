8a3618a4d231c22be15cdd9876477eddfd0cc563
ahmedrehane <rehaneahmed49@gmail.com>
2023-11-27 15:18:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+
+
+
+const App = () =>{
+  return(
+    <div>
+    <nav>
+      <a href="#hello">Home</a>
+      <a href="#2m">News</a>
+      <a href="#rehaneahmed49@gmail.com">Contact</a>
+      <a href="#ahmed.rehane">About</a>
+    </nav>
+    <div class="container">
+      <div class="card">
+        
+        <h1>jean anoulh</h1>
+        <h2>balzac</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est atque beatae voluptates similique dolores placeat quae odit quasi impedit delectus tempora cumque soluta enim, voluptate harum quos mollitia nam provident.
+        </p>
+      </div>
+
+    </div>
+  </div>
+  )
+}
---
+++ src/components/card.jsx
@@ -0,0 +1,14 @@
+
+const card = () =>{
+    return(
+        <div class="card">
+        
+        <h1>jean anoulh</h1>
+        <h2>balzac</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est atque beatae voluptates similique dolores placeat quae odit quasi impedit delectus tempora cumque soluta enim, voluptate harum quos mollitia nam provident.
+        </p>
+      </div>
+
+    )
+}
